721c23d3b1aeb51880ffb88106570d617f559b61
Bruno de Oliveira <corporate.zanin@gmail.com>
2021-01-17 21:11:50 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,7 @@
+def colours(array)
+  colours = %w[yellow blue green violet orange green]
+  colours.each do |cor|
+    puts "Hello i am #{cor} and i have #{cor.length} letters."
+  end 
+end
+
---
+++ looping.rb
@@ -0,0 +1,6 @@
+def looping 
+  loop do 
+    puts "Never pass a school bus if it has flashing red lights"
+  end 
+end 
+looping 
